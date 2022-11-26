8f00360fdfd7ca1882bde6d3fb84cb8fae88b5c6
Niklas Deworetzki <niklas-deworetzki@t-online.de>
2022-11-26 14:55:19 +0100

    Experiment with extension functions.

--- src/main/kotlin/de/thm/mni/compilerbau/utils/StringOps.kt
+++ src/main/kotlin/de/thm/mni/compilerbau/utils/StringOps.kt
@@ -1,7 +1,7 @@
 package de.thm.mni.compilerbau.utils
 
 object StringOps {
-    fun javaLines(str: String): List<String> {
+    private fun javaLines(str: String): List<String> {
         val lines = str.lines()
         return if (str.endsWith("\n"))
             lines.subList(0, lines.size - 1)
@@ -9,9 +9,9 @@ object StringOps {
             lines
     }
 
-    fun indent(str: String, indentation: Int): String {
+    fun String.indented(indentation: Int): String {
         val indentationPrefix = " ".repeat(indentation)
-        return javaLines(str).joinToString("\n") { s -> indentationPrefix + s }
+        return javaLines(this).joinToString("\n") { s -> indentationPrefix + s }
     }
 
     fun toString(o: Any?): String {
