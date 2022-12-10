2c21ea7ad861c6043b11895b814aa080aa392158
Niklas Deworetzki <niklas-deworetzki@t-online.de>
2022-12-10 18:37:45 +0100

    Added some basic executable entry point.

--- src/main/kotlin/de/thm/mni/compilerbau/CommandLineOptions.kt
+++ src/main/kotlin/de/thm/mni/compilerbau/CommandLineOptions.kt
@@ -1,108 +1,49 @@
 package de.thm.mni.compilerbau
 
-import java.io.*
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
+import java.io.File
 
 class CommandLineOptions {
+    @Parameters(
+        index = "0",
+        arity = "1",
+        description = ["The input file containing the source program."]
+    )
+    lateinit var inputFile: File
 
-    enum class PhaseOption {
+    @Parameters(
+        index = "1",
+        arity = "0..1"
+    )
+    var outputFile: File? = null
+
+    @Option(
+        names = ["-h", "--help"],
+        description = ["Displays this help message."]
+    )
+    var helpRequested: Boolean = false
+
+    @Option(
+        names = ["-v", "--version"],
+        description = ["Displays the compiler version."]
+    )
+    var versionRequested: Boolean = false
+
+    enum class DebugPhase {
         TOKENS, PARSE, ABSYN, TABLES, SEMANT, VARS
     }
 
-    @JvmField
-    var phaseOption: PhaseOption? = null
+    @Option(
+        names = ["--debug", "--debug-pass"],
+        description = ["Only executes up to the given pass and produces debug output.",
+            "Supported values are: \${COMPLETION-CANDIDATES}"]
+    )
+    var debug: DebugPhase? = null
 
-    @JvmField
     var ershovOptimization = false
-
-    @JvmField
     var doWhileEnabled = false
-
-    @JvmField
     var firstClassBoolean = false
-
-    @JvmField
     var nestedScopesEnabled = false
 
-    @JvmField
-    var leafProcedureOptimization = false
-
-    // get() = field
-    var inFilename = ""
-    var outFilename = ""
-
-    @JvmName("setPhaseOption1")
-    fun setPhaseOption(phaseOption: PhaseOption?) {
-        if (this.phaseOption != null) usageError("More than one phase option specified.")
-        this.phaseOption = phaseOption
-    }
-
-    companion object {
-        private fun usageError(format: String, vararg args: Any) {
-            System.err.printf("Usage error: $format", *args)
-            System.err.println()
-            showUsage(System.err)
-            System.exit(1)
-        }
-
-        private fun showUsage(out: PrintStream) {
-            out.println("Usage: 'java -jar spl.jar' [OPTION] INPUT_FILE [OUTPUT_FILE]")
-            out.println()
-            out.println("Executes all compiler phases up to (and including) the specified one.")
-            out.println("If no flag is specified, all phases are run and code is written to the output file.")
-            out.println("Options:")
-            out.println("  --tokens     Phase 1: Scans for tokens and prints them.")
-            out.println("  --parse      Phase 2: Parses the stream of tokens to check for syntax errors.")
-            out.println("  --absyn      Phase 3: Creates an abstract syntax tree from the input tokens and prints it.")
-            out.println("  --tables     Phase 4a: Builds a symbol table and prints its entries.")
-            out.println("  --semant     Phase 4b: Performs the semantic analysis.")
-            out.println("  --vars       Phase 5: Allocates memory space for variables and prints the amount of allocated memory.")
-            out.println("  --doWhile    Enable the do-while statement.")
-            out.println("  --bool       Enable boolean variables and logical operators & and |.")
-            out.println("  --scopes     Enable the declaration of variables inside of compound statements.")
-            out.println("  --ershov     Enable register optimization using the ershov number.")
-            out.println("  --leafProc   Enables the optimization of leaf procedures.")
-            out.println("  --help       Show this help.")
-        }
-
-        fun parse(args: Array<String>): CommandLineOptions {
-            val options = CommandLineOptions()
-            for (name in args) {
-                when (name) {
-                    "--tokens" -> options.setPhaseOption(PhaseOption.TOKENS)
-                    "--parse" -> options.setPhaseOption(PhaseOption.PARSE)
-                    "--absyn" -> options.setPhaseOption(PhaseOption.ABSYN)
-                    "--tables" -> options.setPhaseOption(PhaseOption.TABLES)
-                    "--semant" -> options.setPhaseOption(PhaseOption.SEMANT)
-                    "--vars" -> options.setPhaseOption(PhaseOption.VARS)
-                    "--ershov" -> options.ershovOptimization = true
-                    "--doWhile" -> options.doWhileEnabled = true
-                    "--bool" -> options.firstClassBoolean = true
-                    "--scopes" -> options.nestedScopesEnabled = true
-                    "--leafProc" -> options.leafProcedureOptimization = true
-                    "--help" -> {
-                        showUsage(System.out)
-                        System.exit(0)
-                        if (!name.startsWith("--")) {
-                            if (options.inFilename.isEmpty()) options.inFilename =
-                                name else if (options.outFilename.isEmpty()) options.outFilename = name else usageError(
-                                "Too many positional arguments!"
-                            )
-                        } else {
-                            usageError("Unknown option '%s'!", name)
-                        }
-                    }
-
-                    else -> if (!name.startsWith("--")) {
-                        if (options.inFilename.isEmpty()) options.inFilename =
-                            name else if (options.outFilename.isEmpty()) options.outFilename =
-                            name else usageError("Too many positional arguments!")
-                    } else {
-                        usageError("Unknown option '%s'!", name)
-                    }
-                }
-            }
-            if (options.inFilename.isEmpty()) usageError("No input file!")
-            return options
-        }
-    }
 }
--- src/main/kotlin/de/thm/mni/compilerbau/Main.kt
+++ src/main/kotlin/de/thm/mni/compilerbau/Main.kt
@@ -1,115 +1,84 @@
 package de.thm.mni.compilerbau
 
-import de.thm.mni.compilerbau.absyn.Program
 import de.thm.mni.compilerbau.phases._01_scanner.Scanner
+import de.thm.mni.compilerbau.phases._01_scanner.Token
+import de.thm.mni.compilerbau.phases._01_scanner.TokenType
 import de.thm.mni.compilerbau.phases._02_03_parser.Parser
-import de.thm.mni.compilerbau.phases._02_03_parser.Sym
 import de.thm.mni.compilerbau.phases._04a_tablebuild.TableBuilder
 import de.thm.mni.compilerbau.phases._04b_semant.ProcedureBodyChecker
 import de.thm.mni.compilerbau.phases._05_varalloc.VarAllocator
 import de.thm.mni.compilerbau.phases._06_codegen.CodeGenerator
-import de.thm.mni.compilerbau.table.Identifier
-import de.thm.mni.compilerbau.utils.SplError
-import java_cup.runtime.DefaultSymbolFactory
-import java_cup.runtime.Symbol
-import java_cup.runtime.SymbolFactory
-import java.io.FileNotFoundException
-import java.io.FileReader
-import java.io.IOException
+import de.thm.mni.compilerbau.reporting.MessageFormatter
+import de.thm.mni.compilerbau.utils.VersionInfo
+import picocli.CommandLine
+import java.io.FileOutputStream
 import kotlin.system.exitProcess
 
-private fun showToken(token: Symbol) {
-    print("TOKEN = ${Sym.terminalNames[token.sym]}") // Name of token class
-    if (token.sym != Sym.EOF)
-        print(" in line ${token.left}, column ${token.right}") // Line and Column
-    if (token.value != null) {
-        print(", value = ")
-        if (token.value is String || token.value is Identifier)
-            print("\"${token.value}\"")
-        else print(token.value)
+fun main(args: Array<String>) {
+    val options = CommandLineOptions()
+    val cli = CommandLine(options)
+    try {
+        cli.parseArgs(*args)
+        if (options.helpRequested) {
+            cli.usage(System.out)
+            exitProcess(0)
+        } else if (options.versionRequested) {
+            println(VersionInfo.getVersion())
+            exitProcess(1)
+        }
+        exitProcess(run(options))
+    } catch (cliException: CommandLine.ParameterException) {
+        cli.usage(System.out)
+        exitProcess(1)
     }
-    println()
 }
 
-/**
- * CUP encourages you to use {@link java_cup.runtime.ComplexSymbolFactory} as a {@link SymbolFactory} which we
- * don't need. The default implementation provides more than enough information for our needs.
- */
-@Suppress("DEPRECATION")
-var symbolFactory: SymbolFactory = DefaultSymbolFactory()
 
-fun main(args: Array<String>) {
-    val options = CommandLineOptions.parse(args)
-    try {
-        val source = FileReader(options.inFilename)
-        val scanner = Scanner(source)
-        scanner.options = options
-        if (options.phaseOption === CommandLineOptions.PhaseOption.TOKENS) {
-            var token: Symbol
-            do {
-                token = scanner.next_token()
-                showToken(token)
-            } while (token.sym != Sym.EOF)
-            exitProcess(0)
+private fun showTokens(scanner: Scanner) {
+    var token: Token
+    do {
+        token = scanner.nextToken()
+        println(token)
+    } while (token.type != TokenType.EOF)
+}
+
+private fun run(options: CommandLineOptions): Int =
+    Scanner(options.inputFile, options).use { scanner ->
+        if (options.debug === CommandLineOptions.DebugPhase.TOKENS) {
+            showTokens(scanner)
+            return 0
         }
-        val parser = Parser(scanner, symbolFactory)
-        parser.options =
-                options // Inject the command line options into the parser to grant it access to feature flags.
 
-        val program =
-                parser.parse().value as Program // Change 'parse' to 'debug_parse' for detailed parsing output. Don't forget to change it back
-
-        if (options.phaseOption === CommandLineOptions.PhaseOption.PARSE) {
+        val parser = Parser(scanner, options)
+        val program = parser.parse()
+        if (program == null) {
+            for (reportedError in parser.reportedErrors) {
+                println(MessageFormatter.format(reportedError))
+            }
+            println("Syntax Error")
+            return 0
+        } else if (options.debug === CommandLineOptions.DebugPhase.PARSE) {
             println("Input parsed successfully!")
-            exitProcess(0)
-        }
-
-        if (options.phaseOption === CommandLineOptions.PhaseOption.ABSYN) {
+            return 0
+        } else if (options.debug === CommandLineOptions.DebugPhase.ABSYN) {
             println(program)
-            exitProcess(0)
+            return 0
         }
 
         val table = TableBuilder(options).buildSymbolTable(program)
-        if (options.phaseOption == CommandLineOptions.PhaseOption.TABLES) exitProcess(0)
+        if (options.debug == CommandLineOptions.DebugPhase.TABLES) return 0
 
         ProcedureBodyChecker.checkProcedures(program, table)
-        if (options.phaseOption === CommandLineOptions.PhaseOption.SEMANT) {
+        if (options.debug === CommandLineOptions.DebugPhase.SEMANT) {
             println("No semantic errors found!")
-            exitProcess(0)
+            return 0
         }
 
         VarAllocator(options).allocVars(program, table)
-        if (options.phaseOption === CommandLineOptions.PhaseOption.VARS) exitProcess(0)
+        if (options.debug === CommandLineOptions.DebugPhase.VARS) return 0
 
-        try {
-            options.outputWriter.use { out ->
-                CodeGenerator(options, out).generateCode(
-                        program,
-                        table
-                )
-            }
-        } catch (e: IOException) {
-            System.err.println("An error occurred: Cannot open output file '${options.outFilename}'")
-            exitProcess(1)
-        }
-    } catch (e: FileNotFoundException) {
-        System.err.println("An error occurred: Cannot open input file '${options.inFilename}'")
-        exitProcess(1)
-    } catch (error: SplError) {
-        System.err.println("An error occurred:")
-        if (error.position.line >= 0) System.err.printf(
-                "Line %d, Column %d: ",
-                error.position.line,
-                error.position.column
-        )
-        System.err.println(error.message)
-        exitProcess(error.errorCode)
-    } catch (e: Exception) {
-        System.err.println("An error occurred: " + e.message)
-        e.printStackTrace()
-        exitProcess(1)
+        CodeGenerator(options).generateCode(program, table)
+        return 0
     }
-}
-
 
 
