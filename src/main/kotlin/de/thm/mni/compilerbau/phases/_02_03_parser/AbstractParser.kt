08f012bec8f0173acc50549a0b7f2218726296f7
Niklas Deworetzki <niklas-deworetzki@t-online.de>
2022-12-11 21:24:32 +0100

    Implemented parser.

---
+++ src/main/kotlin/de/thm/mni/compilerbau/phases/_02_03_parser/AbstractParser.kt
@@ -0,0 +1,198 @@
+package de.thm.mni.compilerbau.phases._02_03_parser
+
+import de.thm.mni.compilerbau.absyn.Node
+import de.thm.mni.compilerbau.absyn.Node.Companion.withPosition
+import de.thm.mni.compilerbau.phases._01_scanner.Scanner
+import de.thm.mni.compilerbau.phases._01_scanner.Token
+import de.thm.mni.compilerbau.phases._01_scanner.TokenType
+import de.thm.mni.compilerbau.position.Range
+import java.util.*
+import kotlin.collections.ArrayDeque
+
+abstract class AbstractParser<P>(private val scanner: Scanner) {
+    val reportedErrors: MutableList<SyntaxErrorReport> = ArrayList<SyntaxErrorReport>()
+
+    protected var lookahead: Token = scanner.nextToken()
+    protected lateinit var current: Token
+
+    abstract fun describeTokenType(tokenType: TokenType): String
+
+    abstract fun parserEntry(): P
+
+    fun parse(): P? {
+        val result = try {
+            parserEntry()
+        } catch (syntaxError: SyntaxError) {
+            null
+        }
+        if (reportedErrors.isNotEmpty()) return null
+        return result
+    }
+
+    protected fun advance() {
+        current = lookahead
+        lookahead = scanner.nextToken()
+    }
+
+
+    private val positionStack: MutableList<Range> = ArrayDeque()
+
+    protected fun beginParsing() {
+        positionStack.add(0, lookahead.range)
+    }
+
+    protected fun popPosition(): Range =
+        positionStack.removeLast()
+
+    protected fun <P : Node> finishParsing(result: P): P {
+        return result.withPosition(popPosition().union(current.range))
+    }
+
+
+    protected fun <T> parseFully(parser: Parser<T>): T {
+        val result = parser.parse()
+        if (lookahead.type !== TokenType.EOF) {
+            unrecoverableError(current.range, "end of file")
+        }
+        return result
+    }
+
+    protected fun reentryAt(tokenTypes: Set<TokenType>) {
+        while (!tokenTypes.contains(lookahead.type)) {
+            if (lookahead.type === TokenType.EOF) throw SyntaxError()
+            advance()
+        }
+    }
+
+    protected fun isPresent(tokenType: TokenType): Boolean =
+        if (lookahead.type === tokenType) {
+            advance()
+            true
+        } else false
+
+    protected fun token(tokenType: TokenType, expected: String? = null): Unit =
+        if (lookahead.type === tokenType) advance()
+        else {
+            popPosition()
+            error(lookahead.range, expected ?: tokenType)
+        }
+
+    protected inline fun <reified T> tokenAs(tokenType: TokenType, expected: String? = null): T =
+        if (lookahead.type === tokenType) {
+            advance()
+            current.value as T
+        } else {
+            popPosition()
+            error(lookahead.range, expected ?: tokenType)
+        }
+
+    fun <T> parseRepeated(parser: Parser<T>, firstSet: Set<TokenType>, followSet: Set<TokenType>): List<T> {
+        val results: MutableList<T> = mutableListOf()
+
+        while (lookahead.type !in followSet) {
+            try {
+                results.add(parser.parse())
+            } catch (recovery: Recovery) {
+                reentryAt(firstSet.union(followSet))
+            }
+        }
+
+        return results
+    }
+
+    protected fun <T> parseSepList(separator: TokenType, terminator: TokenType, parser: Parser<T>): List<T> {
+        if (lookahead.type === terminator) {
+            advance()
+            return emptyList()
+        }
+
+        val results: MutableList<T> = ArrayList()
+        do {
+            if (results.isNotEmpty()) advance() // Skip separator.
+
+            try {
+                results.add(parser.parse())
+            } catch (recovery: Recovery) {
+                if (lookahead.type === separator || lookahead.type === terminator) {
+                    advance() // Prevent being stuck on a separator without consuming input.
+                }
+                reentryAt(setOf(separator, terminator))
+            }
+        } while (lookahead.type === separator)
+
+        if (lookahead.type !== terminator) {
+            reportError(lookahead.range, listOf(describeTokenType(separator), describeTokenType(terminator)))
+            reentryAt(setOf(terminator))
+        }
+        advance()
+        return results
+    }
+
+    protected fun <N : Node, O> parseExpressionHierarchy(
+        constructor: (N, O, N) -> N,
+        lowerLevel: Parser<N>,
+        vararg operators: Pair<TokenType, O>
+    ): N {
+        val operatorMap = operators.toMap()
+        var result = lowerLevel.parse()
+        val beginPosition = result.position
+
+        while (lookahead.type in operatorMap.keys) {
+            val operator = operatorMap[lookahead.type]!!
+            advance()
+            val rhs = lowerLevel.parse()
+            result = constructor(result, operator, rhs)
+                .withPosition(beginPosition.union(rhs.position))
+        }
+        return result
+    }
+
+    /**
+     * An interface modelling a parser for combinator methods.
+     * <p>
+     * Parsers have a single method that returns some parsed
+     * element while consuming input as a side effect. A parser
+     * may report a {@link SyntaxError} during parsing.
+     *
+     * @param <R> Type of elements returned by the parser.
+     */
+    fun interface Parser<R> {
+        fun parse(): R
+    }
+
+
+    /**
+     * Creates a new {@link SyntaxErrorReport} and returns <code>true</code> when the
+     * error limit is exceeded. Otherwise, <code>false</code> is returned.
+     */
+    private fun reportError(range: Range, expected: List<String>) {
+        reportedErrors.add(SyntaxErrorReport(range, expected));
+    }
+
+    protected fun error(range: Range, vararg expected: Any): Nothing {
+        reportError(range, formatExpected(expected))
+        throw Recovery()
+    }
+
+    protected fun unrecoverableError(range: Range, vararg expected: Any): Nothing {
+        reportError(range, formatExpected(expected))
+        throw SyntaxError()
+    }
+
+    private fun formatExpected(expected: Array<out Any>): List<String> =
+        expected.map {
+            if (it is TokenType) describeTokenType(it)
+            else Objects.toString(it)
+        }
+
+
+    /**
+     * Special {@link Exception} class indicating that error recovery can be attempted.
+     */
+    class Recovery : SyntaxError()
+
+    /**
+     * Special {@link Exception} class indicating that parsing should be aborted.
+     */
+    open class SyntaxError : Exception()
+}
---
+++ src/main/kotlin/de/thm/mni/compilerbau/phases/_02_03_parser/Parser.kt
@@ -0,0 +1,255 @@
+package de.thm.mni.compilerbau.phases._02_03_parser
+
+import de.thm.mni.compilerbau.CommandLineOptions
+import de.thm.mni.compilerbau.absyn.*
+import de.thm.mni.compilerbau.absyn.Node.Companion.withPosition
+import de.thm.mni.compilerbau.phases._01_scanner.Scanner
+import de.thm.mni.compilerbau.phases._01_scanner.TokenType
+import de.thm.mni.compilerbau.phases._01_scanner.TokenType.*
+import de.thm.mni.compilerbau.table.Identifier
+
+class Parser(scanner: Scanner, val options: CommandLineOptions) : AbstractParser<Program>(scanner) {
+
+    override fun describeTokenType(tokenType: TokenType): String = tokenType.toString()
+
+    override fun parserEntry(): Program = parseProgram()
+
+    private fun parseProgram(): Program {
+        beginParsing()
+        val declarations = parseRepeated(this::parseGlobalDeclaration, setOf(TYPE, PROC), setOf(EOF))
+        return finishParsing(Program(declarations))
+    }
+
+    private fun parseGlobalDeclaration(): GlobalDeclaration = when (lookahead.type) {
+        TYPE -> {
+            beginParsing()
+            advance()
+
+            val identifier = parseIdentifier()
+            token(OP_EQ)
+            val typeExpression = parseTypeExpression()
+            token(SEMIC)
+
+            finishParsing(TypeDeclaration(identifier, typeExpression))
+        }
+
+        PROC -> {
+            beginParsing()
+            advance()
+
+            val identifier = parseIdentifier()
+            token(PAR_L)
+            val parameters = parseSepList(COMMA, PAR_R, ::parseParameterDeclaration)
+
+            token(CUR_L)
+            val variables = mutableListOf<VariableDeclaration>()
+            while (lookahead.type === VAR)
+                variables.add(parseVariableDeclaration())
+            val statements = mutableListOf<Statement>()
+            while (lookahead.type !== CUR_R)
+                statements.add(parseStatement())
+            token(CUR_R)
+
+            finishParsing(ProcedureDeclaration(identifier, parameters, variables, statements))
+        }
+
+        else ->
+            error(lookahead.range, "global declaration")
+    }
+
+
+    private fun parseIdentifier(): Identifier =
+        Identifier(tokenAs(IDENTIFIER, "identifier"))
+
+    private fun parseTypeExpression(): TypeExpression = when (lookahead.type) {
+        ARRAY -> {
+            beginParsing()
+            advance()
+            token(BRA_L)
+            val size = tokenAs<Int>(INTEGER, "array size")
+            token(BRA_R)
+            token(OF)
+            val base = parseTypeExpression()
+            finishParsing(ArrayTypeExpression(size, base))
+        }
+
+        IDENTIFIER -> {
+            beginParsing()
+            val typename = parseIdentifier()
+            finishParsing(NamedTypeExpression(typename))
+        }
+
+        else ->
+            error(lookahead.range, "type expression")
+    }
+
+    private fun parseParameterDeclaration(): ParameterDeclaration {
+        beginParsing()
+        val isReference = isPresent(REF)
+        val name = parseIdentifier()
+        token(COLON)
+        val type = parseTypeExpression()
+        return finishParsing(ParameterDeclaration(name, type, isReference))
+    }
+
+    private fun parseVariableDeclaration(): VariableDeclaration {
+        beginParsing()
+        token(VAR)
+        val name = parseIdentifier()
+        token(COLON)
+        val type = try {
+            val result = parseTypeExpression()
+            token(SEMIC)
+            result
+        } catch (recovery: Recovery) {
+            reentryAt(setOf(SEMIC, CUR_R))
+            if (lookahead.type === SEMIC) advance()
+            beginParsing() // Restart parsing.
+            NamedTypeExpression(Identifier("Int"))
+        }
+        return finishParsing(VariableDeclaration(name, type))
+    }
+
+    private fun parseStatement(): Statement = when (lookahead.type) {
+        CUR_L -> {
+            beginParsing()
+            advance()
+            val statements = parseRepeated(::parseStatement, emptySet(), setOf(CUR_R))
+            advance()
+            finishParsing(CompoundStatement(statements))
+        }
+
+        IF -> {
+            beginParsing()
+            advance()
+            token(PAR_L)
+            val condition = parseExpression()
+            token(PAR_R)
+            val thenBranch = parseStatement()
+            val elseBranch = if (isPresent(ELSE)) parseStatement() else EmptyStatement().withPosition(current.range)
+            finishParsing(IfStatement(condition, thenBranch, elseBranch))
+        }
+
+        WHILE -> {
+            beginParsing()
+            advance()
+            token(PAR_L)
+            val condition = parseExpression()
+            token(PAR_R)
+            val body = parseStatement()
+            finishParsing(WhileStatement(condition, body))
+        }
+
+        IDENTIFIER -> {
+            beginParsing()
+            val name = parseIdentifier()
+            if (lookahead.type === PAR_L) {
+                advance()
+                val arguments = parseSepList(COMMA, PAR_R, ::parseExpression)
+                token(SEMIC)
+                finishParsing(CallStatement(name, arguments))
+            } else {
+                val target = parseArrayAccesses(NamedVariable(name).withPosition(current.range))
+                token(ASSIGN)
+                val value = parseExpression()
+                token(SEMIC)
+                finishParsing(AssignStatement(target, value))
+            }
+        }
+
+        SEMIC -> {
+            beginParsing()
+            advance()
+            finishParsing(EmptyStatement())
+        }
+
+        else -> error(current.range, "statement")
+    }
+
+    private fun parseVariable(): Variable {
+        beginParsing()
+        val beginPosition = current.range
+
+        val name = parseIdentifier()
+        val base = NamedVariable(name).withPosition(beginPosition)
+        val variable = parseArrayAccesses(base)
+
+        popPosition()
+        return variable
+    }
+
+    private fun parseArrayAccesses(base: Variable): Variable {
+        var result: Variable = base
+        while (lookahead.type === BRA_L) {
+            advance()
+            val index = parseExpression()
+            token(BRA_R)
+
+            result = ArrayAccess(result, index)
+                .withPosition(base.position.union(current.range))
+        }
+        return result
+    }
+
+
+    private fun parseExpression(): Expression = parseComparison()
+
+    private fun parseComparison(): Expression = parseExpressionHierarchy(
+        ::BinaryExpression,
+        ::parseAddition,
+        OP_EQ to BinaryExpression.Operator.EQU,
+        OP_NE to BinaryExpression.Operator.NEQ,
+        OP_LT to BinaryExpression.Operator.LST,
+        OP_LE to BinaryExpression.Operator.LSE,
+        OP_GT to BinaryExpression.Operator.GRT,
+        OP_GE to BinaryExpression.Operator.GRE,
+    )
+
+    private fun parseAddition(): Expression = parseExpressionHierarchy(
+        ::BinaryExpression,
+        ::parseMultiplication,
+        OP_ADD to BinaryExpression.Operator.ADD,
+        OP_SUB to BinaryExpression.Operator.SUB,
+    )
+
+    private fun parseMultiplication(): Expression = parseExpressionHierarchy(
+        ::BinaryExpression,
+        ::parseUnaryExpression,
+        OP_MUL to BinaryExpression.Operator.MUL,
+        OP_DIV to BinaryExpression.Operator.DIV,
+    )
+
+    private fun parseUnaryExpression(): Expression = when (lookahead.type) {
+        OP_SUB -> {
+            beginParsing()
+            advance()
+            val operand = parseAtomicExpression()
+            finishParsing(UnaryExpression(UnaryExpression.Operator.MINUS, operand))
+        }
+
+        else -> parseAtomicExpression()
+    }
+
+    private fun parseAtomicExpression(): Expression = when (lookahead.type) {
+        INTEGER -> {
+            beginParsing()
+            val value = tokenAs<Int>(INTEGER)
+            finishParsing(IntLiteral(value))
+        }
+
+        IDENTIFIER -> {
+            val variable = parseVariable()
+            VariableExpression(variable).withPosition(variable.position)
+        }
+
+        PAR_L -> {
+            advance()
+            val result = parseExpression()
+            token(PAR_R)
+            result
+        }
+
+        else ->
+            error(current.range, "expression")
+    }
+}
